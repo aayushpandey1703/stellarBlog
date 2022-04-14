990e51f776b5c88184e509933f4b5d0349149758
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2022-04-14 14:34:21 +0530

    Add files via upload

---
+++ login1.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="login.php" method="post"/>
+         <p><input type="email" name="email" value="" placeholder="enter email"/></p>
+          <p><input type="text" name="password" value="" placeholder="enter password"/></p>
+           <p><input type="submit" name="login" value="login"/></p>
+           <a href="register1.php">register</a>
+    </form>
+    </body>
+</html>
