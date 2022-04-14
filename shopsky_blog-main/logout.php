ad01e0b04abe80c6635272536e8dfebff1118902
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2022-04-14 14:34:45 +0530

    Delete shopsky_blog-main directory

--- shopsky_blog-main/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-    session_unset();
-    session_destroy();
-    echo "<script>alert('Logget out successful');window.location.href = 'index.php';</script>";
-    die();
-
-?>
