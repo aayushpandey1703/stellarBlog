7c8eea5018a53def3af3e137d6bc27d034cba4be
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2022-04-14 16:24:17 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$host='localhost';
-$user='root';
+$host='stellarblog-server.mysql.database.azure.com';
+$user='vjimpvyxwf';
 $db='shopsky_blog';
-$pass='';
+$pass='A@yush1703';
 $con=  mysqli_connect($host, $user, $pass, $db);
 
 if(!$con)
